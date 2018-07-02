afc36e006a13ac8a70f0cd54ce5be1766509afe7
Esteban D. Barboza Primera <dario0117@gmail.com>
2018-07-02 12:48:17 -0500

    v1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# metascrapper-implementation
+# Metascrapper Implementation
+
+## Usage
+
+```http
+POST http://localhost:5000
+content-type: application/json
+
+{
+    "url": "https://github.com/Dario0117/metascrapper-implementation"
+}
+```
+
+For more information about the response, check metascraper web site: [https://metascraper.js.org/#/](https://metascraper.js.org/#/)
+
---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const metascraper = require('metascraper');
+const got = require('got');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send(`
+    For more information visit <a target="_blank" href="https://github.com/Dario0117/metascrapper-implementation">https://github.com/Dario0117/metascrapper-implementation</a>
+    `)
+})
+
+app.post('/', async (req, res) => {
+    let { url } = req.body;
+    const { body: html } = await got(url)
+    const metadata = await metascraper({ html, url })
+    res.json(metadata);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started at ${PORT}`);
+});
